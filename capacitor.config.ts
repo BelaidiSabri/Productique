459d6217ad451b78003e6d4504c88fafcfc66aa2
Belaidi Sabri <belaidisabri@gmail.com>
2023-08-17 09:26:39 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'produtique',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
